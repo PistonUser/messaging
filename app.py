2b34ece9e953faca2b1ee4ba162e78b9733ed59a
PistonUser <88914090+PistonUser@users.noreply.github.com>
2023-12-12 16:12:01 +0000

    y

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 # python -m pip install flask
 # python -m flask run
 from flask import Flask, render_template, request, redirect
+from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
-messages = []
 
 @app.route("/", methods=["GET","POST"])
 def home():
-    if request.method == "POST":
-      messages += [{"name":request.form.get("user"),"message":request.form.get("message")}]
-    return render_template("index.html", messages=messages)
+    with sqlite3.connect("message.db") as conn:
+      c = conn.cursor
+      if request.method == "POST":
+        c.execute("INSERT INTO messages VALUES (?,?)",(request.form.get("user"), request.form.get("message")))
+        conn.commit()
+      messages = c.execute("SELECT * FROM messages")
+      return render_template("index.html", messages=messages)
 
-@app.route("/<point1>", methods=["GET","POST"])
-def drew(point1):
-    if request.method == "POST":
-        point = request.form.get("draw")
-        return redirect(f"/{point}")
-    else:
-        list = grid()
-        return render_template("drew.html", grid=list.grid, point=point1)
+# @app.route("/<point1>", methods=["GET","POST"])
+# def drew(point1):
+#     if request.method == "POST":
+#         point = request.form.get("draw")
+#         return redirect(f"/{point}")
+#     else:
+#         return render_template("drew.html", point=point1)
